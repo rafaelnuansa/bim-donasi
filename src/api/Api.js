42e8470729e5da9bf56cc03eb152c512a88316a2
rafaelnuansa <iandrewnew@gmail.com>
2023-12-03 18:16:03 +0700

    api

--- src/api/Api.js
+++ src/api/Api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 const Api = axios.create({
     //set default endpoint API
-    baseURL: 'http://localhost:8000/api'
+    baseURL: 'https://backend-donasi.binaimanmulia.id/api'
 })
 
 export default Api
